c71e5725e1c601da54b99ad2424eb98593bff8f6
KyleLearnedThis <kyle.learned.this@gmail.com>
2017-06-28 23:49:31 -0700

    Commit 2

--- src/main/java/com/albion/githug/Widget.java
+++ src/main/java/com/albion/githug/Widget.java
@@ -3,5 +3,6 @@ package com.albion.githug;
 public class Widget {
     public static void main(String[] args) {
         System.out.println("Line 0001");
+        System.out.println("Line 0002");
     }
 }
